a61e817d33e303989f889d51f31bbabbf79483b4
ohhAndy <andy.hujze@gmail.com>
2025-11-04 23:38:10 -0500

    fix(backend): fixed small bug preventing redirect to frontend

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,17 +1,24 @@
 import { Router } from "express";
 import passport from "../config/passport";
 import * as authCtrl from "../controllers/authController";
-import { sanitizeName, checkEmail } from "../middlewares/validate"
+import { sanitizeName, checkEmail } from "../middlewares/validate";
 
 const router = Router();
 
-router.post('/register', sanitizeName, checkEmail, authCtrl.register);
-router.post('/login', checkEmail, authCtrl.login);
-router.post('/logout', authCtrl.logout);
-router.get('/profile', authCtrl.profile);
-router.get('/google', passport.authenticate("google", { scope: ["profile", "email"] }));
-router.get('/google/callback', passport.authenticate("google", { failureRedirect: `/` }), 
-  (req, res) => { if (req.user && req.user.id) req.session.userId = req.user.id; },
-  authCtrl.googleRedirect
+router.post("/register", sanitizeName, checkEmail, authCtrl.register);
+router.post("/login", checkEmail, authCtrl.login);
+router.post("/logout", authCtrl.logout);
+router.get("/profile", authCtrl.profile);
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: `/` }),
+  (req, res) => {
+    if (req.user && req.user.id) req.session.userId = req.user.id;
+    authCtrl.googleRedirect;
+  }
 );
 export default router;
