60d700d72d2b15b11bc03844a59a1fc218d86e84
Jean Luc Imanishimwe <imanjean30@gmail.com>
2025-11-05 21:31:43 -0500

    none and trust proxy needed for now secure later

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,14 +22,14 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      sameSite: "lax",
+      sameSite: "none",
       secure: process.env.NODE_ENV == "prod", // sets the secure flag only with HTTPS in production
       httpOnly: true
     },
   })
 );
 
-// app.set("trust proxy", 1);
+app.set("trust proxy", 1);
 
 app.use(
   cors({
