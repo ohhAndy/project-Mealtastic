eab09530a8dd50806c5ec362ea6310f793b43f6c
ohhAndy <andy.hujze@gmail.com>
2025-11-12 21:23:24 -0500

    fix small bug

--- backend/src/routes/roomsRoutes.ts
+++ backend/src/routes/roomsRoutes.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.post("/", isAuthenticated, roomCtrl.createRoom);
 router.get("/", isAuthenticated, roomCtrl.getRooms);
 router.post("/:roomId/join", isAuthenticated, roomCtrl.joinRoom);
-router.get("/api/rooms/:roomId/poll", roomCtrl.pollRoom); 
+router.get("/:roomId/poll", roomCtrl.pollRoom); 
 router.post("/:roomId/signal", isAuthenticated, roomCtrl.signalRoom);
 router.post("/:roomId/leave/:peerId", isAuthenticated, roomCtrl.leaveRoom);
 router.delete("/:roomId", isAuthenticated, roomCtrl.deleteRoom);
